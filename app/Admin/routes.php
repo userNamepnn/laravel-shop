4a52863ffc543b19f9cdb4c822ae501c145a1af8
潘倪楠 <982397045@qq.com>
2019-02-16 11:16:41 +0800

    管理后台 -新增,修改商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,6 +12,12 @@ Route::group([
 
     $router->get('/', 'HomeController@index');
     $router->get('/users', 'UsersController@index');
+
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
+
     $router->get('/products', 'ProductsController@index');
 
 });
