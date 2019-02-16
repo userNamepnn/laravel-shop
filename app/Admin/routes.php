d2304abf4c5b6d4d02a03da0f04da28571d7f889
潘倪楠 <982397045@qq.com>
2019-02-16 14:23:09 +0800

    用户界面 -商品筛选,排序

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -9,15 +9,19 @@ Route::group([
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
-
+    //管理后台主页
     $router->get('/', 'HomeController@index');
+    //管理员列表
     $router->get('/users', 'UsersController@index');
 
+    //商品添加
     $router->get('products/create', 'ProductsController@create');
     $router->post('products', 'ProductsController@store');
+    //商品修改
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
 
+    //商品列表
     $router->get('/products', 'ProductsController@index');
 
 });
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,16 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
+Route::get('products', 'ProductsController@index')->name('products.index');
+
 Route::group(['middleware' => ['auth', 'verified']], function () {
+
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     Route::get('user_addresses/{address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
+
 });
