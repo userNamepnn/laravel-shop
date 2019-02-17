29f7f12b25b6637734e8a5b4c8fd73ea25bbd5d7
潘倪楠 <982397045@qq.com>
2019-02-17 16:06:11 +0800

    引入支付宝

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -47,6 +47,12 @@ class OrdersController extends Controller
         return view('orders.index', compact('orders'));
     }
 
+    /**
+     * 订单详情
+     * @param Order $order
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Order $order, Request $request)
     {
         $this->authorize('own', $order);
